86473243a93dc376490e96283ba275ba1ea0d7db
anajungg <ana_l_jung@estudante.sesisenai.org.br>
2026-03-30 09:26:09 -0300

    add- fiz atualizações devido ao erro do meu visual, atualizado e na versão final.

--- script.js
+++ script.js
@@ -14,22 +14,29 @@ form.addEventListener('submit', function (e) {
     let cidade = document.getElementById('cidade').value;
     let moradia = document.getElementById('moradia').value;
     let quintal = document.getElementById('quintal').value;
+    let horas = document.getElementById('horas').value;
+    let animal = document.getElementById('animal').value;
+    let termo = document.getElementById('termo').checked;
+    let condiçoes = document.getElementById('condiçoes').value;
+    let decisao = document.getElementById('decisao').value;
+    let local = document.getElementById('local').value;
+    let motivo = document.getElementById('motivo').value;
 
     document.getElementById('erroNome').textContent = '';
     document.getElementById('erroEmail').textContent = '';
 
-    if (nome.length < 3) {
-        document.getElementById('erroNome').textContent = 'Nome deve ter pelo menos 3 caracteres.';
-        valido = false;
-    }
-
-    if (!email.includes('@')) {
-        document.getElementById('erroEmail').textContent = 'Email Inválido, deve conter @.';
-        valido = false;
-    }
-    if (telefone.length < 8) {
-    alert("Telefone inválido.");
+if (nome.length < 3) {
+    document.getElementById('erroNome').textContent = 'Nome deve ter pelo menos 3 caracteres.';
     valido = false;
+}
+
+if (!email.includes('@')) {
+document.getElementById('erroEmail').textContent = 'Email Inválido, deve conter @.';
+valido = false;
+}
+if (telefone.length < 8) {
+alert("Telefone inválido.");
+valido = false;
 }
 if (idade === "" || idade <= 0) {
     alert("Informe uma idade válida.");
@@ -45,10 +52,9 @@ if (cpf === "") {
 }
     let cpfsExistentes = ["12345678900", "11111111111"];
 
-<<<<<<< HEAD
     if (valido) {
         let resultado = document.getElementById('resultado');
-=======
+    }
 if (cpfsExistentes.includes(cpf)) {
     alert("CPF já cadastrado!");
     valido = false;
@@ -61,19 +67,14 @@ if (cidade === "") {
     alert("Cidade é obrigatória.");
     valido = false;
 }
-let horas = document.getElementById('horas').value;
 
 if (horas > 8) {
     alert("O animal ficará muito tempo sozinho! Justifique melhor.");
 }
->>>>>>> 5048969b09a65c5fc50fcb81fe6adaf2cd36fdc6
-
-let animal = document.getElementById('animal').value;
 
 if (animal === "naoteve") {
     alert("A ONG poderá acompanhar sua adaptação com o animal.");
 }
-let termo = document.getElementById('termo').checked;
 
 if (!termo) {
     alert("Você precisa aceitar o termo.");
@@ -84,28 +85,24 @@ if (moradia === "apartamento" && quintal === "sim") {
     alert("Quem mora em apartamento não pode ter quintal!");
     valido = false;
 }
-let condiçoes = document.getElementById('condiçoes').value;
 
 if (financeiro === "ruim") {
     alert("Você precisa ter condições financeiras para adotar.");
     valido = false;
 }
-    let decisao = document.getElementById('decisao').value;
 
 if (decisao === "hoje") {
     alert("Cuidado com decisões impulsivas!");
 }
-let extra = document.getElementById('extra').value;
 
-if (moradia === "apartamento" && extra !== "permite") {
+if (moradia === "apartamento" && local !== "permite") {
     alert("Apartamento deve permitir animais!");
     valido = false;
 }
-if (moradia === "casa" && extra !== "seguro") {
+if (moradia === "casa" && local !== "seguro") {
     alert("A casa precisa ter um quintal seguro!");
     valido = false;
 }
-let motivo = document.getElementById('motivo').value;
 
 if (motivo.length < 10 || motivo === "quero" || motivo === "porque sim") {
     alert("Explique melhor o motivo da adoção.");
